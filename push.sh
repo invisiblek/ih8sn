ac036e5f8fe0c9ac0f3db6a7e94aedf5e645c7e7
Dan Pasanen <dan.pasanen@gmail.com>
2021-01-26 13:38:58 -0600

    add conf for product type

--- push.sh
+++ push.sh
@@ -7,8 +7,11 @@ adb wait-for-device push ih8sn /system/bin/
 adb wait-for-device push ih8sn.rc /system/etc/init/
 
 SERIALNO=$(adb shell getprop ro.boot.serialno)
+PRODUCT=$(adb shell getprop ro.build.product)
 
-if [[ -f "ih8sn.conf.${SERIALNO}" ]]; then
+if [[ -f "ih8sn.conf.${PRODUCT}" ]]; then
+    adb wait-for-device push ih8sn.conf.${PRODUCT} /system/etc/ih8sn.conf
+elif [[ -f "ih8sn.conf.${SERIALNO}" ]]; then
     adb wait-for-device push ih8sn.conf.${SERIALNO} /system/etc/ih8sn.conf
 else
     adb wait-for-device push ih8sn.conf /system/etc/
